09e519989415c212b246907fba44e3aee066b4fe
nephros <nemo@pgxperiiia10>
2023-08-23 12:53:58 +0200

    Do not return duplicate names if identical

--- src/chumpackage.cpp
+++ src/chumpackage.cpp
@@ -37,8 +37,12 @@ ChumPackage::ChumPackage(const QString &id, QObject *parent)
 }
 
 static QString nameFormatting(const QString &name, const QString &login) {
-    if (!login.isEmpty() && !name.isEmpty())
+    if (!login.isEmpty() && !name.isEmpty()) {
+        if (QString::compare(login, name, Qt::CaseSensitive) == 0) {
+            return login;
+        }
         return QStringLiteral("%1 (%2)").arg(name, login);
+    }
     if (!login.isEmpty()) return login;
     if (!name.isEmpty()) return name;
     return QString();
